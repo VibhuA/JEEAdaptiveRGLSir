c80175c024c0efc4efdf7246096897008152a548
VibhuA <68466801+VibhuA@users.noreply.github.com>
2026-04-06 13:22:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -213,6 +213,7 @@ else:
                 st.markdown(f"**User Ans:** :{color}[{res['user_val']}] | **Correct:** {res['correct_val']}")
                 
                 # --- NEW: DISPLAY SOLUTION IMAGE ---
+                st.write(f"DEBUG: Looking for file at {os.path.abspath(sol_path)}")
                 sol_path = res.get('sol_img', '')
                 if sol_path and os.path.exists(sol_path):
                     st.divider()
